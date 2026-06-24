836124722b6937b02fc6a48489cdc872c46f4260
skipp-dev <preuss.steffen@yahoo.com>
2026-06-24 22:47:17 +0200

    test(config): disable benzinga_rss in active_sources isolation cases

--- tests/test_newsstack_fmp.py
+++ tests/test_newsstack_fmp.py
@@ -1931,6 +1931,7 @@ class TestConfigActiveSources(unittest.TestCase):
             "ENABLE_FMP_GENERAL": "0",
             "ENABLE_BENZINGA_REST": "0",
             "ENABLE_BENZINGA_WS": "0",
+            "ENABLE_BENZINGA_RSS": "0",
             "ENABLE_TRADINGVIEW_NEWS": "0",
             "ENABLE_NEWSAPI_AI": "0",
         }):
@@ -1959,6 +1960,7 @@ class TestConfigActiveSources(unittest.TestCase):
             "ENABLE_FMP": "0",
             "ENABLE_BENZINGA_REST": "0",
             "ENABLE_BENZINGA_WS": "0",
+            "ENABLE_BENZINGA_RSS": "0",
             "ENABLE_TRADINGVIEW_NEWS": "0",
             "ENABLE_FMP_ARTICLES": "0",
             "ENABLE_FMP_GENERAL": "0",
@@ -1977,6 +1979,7 @@ class TestConfigActiveSources(unittest.TestCase):
             "ENABLE_FMP_GENERAL": "0",
             "ENABLE_BENZINGA_REST": "0",
             "ENABLE_BENZINGA_WS": "0",
+            "ENABLE_BENZINGA_RSS": "0",
             "ENABLE_TRADINGVIEW_NEWS": "0",
             "ENABLE_NEWSAPI_AI": "0",
         }):
